f820ab5a679e7593b62851e7fd1acb426806e4aa
Sai Kumar <163011196+Saikumar0005@users.noreply.github.com>
2025-08-31 16:15:00 +0530

    left.java

---
+++ Pyramid/left.java
@@ -0,0 +1,44 @@
+class left{
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int s=1;s<=(n-i);s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Output:-
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+
+class leftinverted{
+    public static void main(String[] args){
+        int n=5;
+        for(int i=n;i>=1;i--){
+            for(int s=1;s<=(n-i);s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Output:-
+// *****
+//  ****
+//   ***
+//    **
+//     *
