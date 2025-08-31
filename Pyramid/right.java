0705e563fa2d06ae4d1ea01e8ca57a5828547611
Sai Kumar <163011196+Saikumar0005@users.noreply.github.com>
2025-08-31 16:10:51 +0530

    right.java

--- Pyramid/right.java
+++ Pyramid/right.java
@@ -9,3 +9,29 @@ class right{
         }
     }
 }
+
+// Output:-
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+
+class right{
+    public static void main(String[] args){
+        int n=5;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Output:-
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
