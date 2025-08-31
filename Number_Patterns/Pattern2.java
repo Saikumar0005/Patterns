073147d7c9d5f82f9655ba3b4d3c60506acee534
Sai Kumar <163011196+Saikumar0005@users.noreply.github.com>
2025-08-31 16:32:11 +0530

    Create Pattern2.java

---
+++ Number_Patterns/Pattern2.java
@@ -0,0 +1,20 @@
+class number{
+    public static void main(String[] args){
+        int n=4;
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num +" ");
+                num++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
+// Output:-
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
