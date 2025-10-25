61d3f8a2b88a5dbed62f6a837eaad6cb6d58fd79
Sai Kumar <163011196+Saikumar0005@users.noreply.github.com>
2025-10-26 00:41:09 +0530

    Update diamond.java

--- Pyramid/diamond.java
+++ Pyramid/diamond.java
@@ -33,3 +33,47 @@ class diamond{
 //   * * * 
 //    * * 
 //     * 
+
+
+
+
+class DiamondShape {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            // Print ascending numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            // Print descending numbers
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            // Print ascending numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            // Print descending numbers
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
